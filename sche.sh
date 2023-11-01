433c5f05a3de3473bcadf693f2cdc1021b9b5f70
Hunter1 <148002427+HunterTekaj@users.noreply.github.com>
2023-11-01 12:02:52 -0300

    Update and rename sche to sche.sh

--- sche.sh
+++ sche.sh
@@ -4,6 +4,7 @@ title="(sche)"
 schefile="$HOME/.config/sche/config.txt"
 curtime=$(date +"%H.%M") # hour.minutes
 stime=$((300)) # sleep_time; default: 5 minutes
+acttime=$((1800*1000)) # 30 minutes * 1 mil second in seconds
 
 if [[ -z "$1" || "$1" == "-h" || "$1" == "--help" ]]; then
     echo "Usage: sche <none> [s|start] [w|write] [a|all]"
@@ -18,14 +19,18 @@ elif [[ "$1" == "s" || "$1" == "start" ]]; then
             loadm=$(cat "$schefile" | grep "$(date +%H):")
         fi
         sleep $stime
-        notify-send -t 1800000 "Sche" "$(cat "$schefile" | grep "$curtime:" )"
+        notify-send -t $acttime "Sche" $(cat "$schefile" | grep "$curtime:" )
     done
 
 elif [[ "$1" == "a" || "$1" == "all" ]]; then
     cat "$schefile" | grep -v "#" | grep .
 
 elif [[ "$1" == "w" || "$1" == "write" ]]; then
-    $EDITOR $schefile
+    if [ -z "$EDITOR" ]; then
+        echo "sche: error: no EDITOR variable set!"
+    else
+        $EDITOR $schefile
+    fi
 fi
 
 # else: just exit with no output
